ccdf1242483bef1ce39003bd961b8dc2a20eea3b
andre.ofuji <andre.ofuji@gmail.com>
2021-05-29 14:42:09 -0300

    Configuracao inicial

---
+++ src/components/main/index.tsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+
+            <h1>Meio</h1>
+
+        </main>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Footer from '../components/footer'
+import Header from '../components/header'
+import Main from '../components/main'
+
+
+export default function Home() {
+
+  console.log('asdasd')
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Gerador de frases</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
