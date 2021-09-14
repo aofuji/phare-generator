e00a35f32e88b9f7598e7d2e9bfa7c3ce65a86eb
andre.ofuji <andre.ofuji@gmail.com>
2021-09-13 21:44:34 -0300

    Alterado path da imagem

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,15 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-indigo-600 items-center w-full h-24 border-t flex flex-col justify-center">            
-                <div className="w-1/3 flex justify-center">
-                    <img className="object-contain w-14" src="https://play-lh.googleusercontent.com/JhlUkFmf_qYUMEV_H-WPvG_TMB7oiSY8jqqRIkzJ_cZRe0eWYIx_x59NPMX-gMpUSRZN=s128-rw" alt="" />
-                    <p className="w-13 text-lg text-white ml-3 justify-center">
-                    Gostou? Se quiser ajudar doe para manter o <br/>
+        <footer className="bg-indigo-600 items-center w-full h-24 border-t flex flex-col justify-center">
+            <div className="w-1/3 flex justify-center">
+                <img className="object-contain " width={80}
+                    height={80} src="https://gruporgm.com.br/wp-content/uploads/2021/09/qr-code-pix-gerador-frases.png" alt="" />
+                <p className="w-13 text-lg text-white ml-3 mt-3 ">
+                    Gostou? Se quiser ajudar doe para manter o <br />
                     projeto sempre funcionando e atualizado. Obrigado!
-                    </p>
-                </div>  
+                </p>
+            </div>
         </footer>
     )
 }
