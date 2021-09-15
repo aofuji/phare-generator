0b7f1f578fc0f6cb6354b71e00f8c4c02370abed
andre.ofuji <andre.ofuji@gmail.com>
2021-09-14 22:08:18 -0300

    Adicionado Goolge analitycs

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <footer className="bg-indigo-600 items-center w-full h-10 border-t flex flex-col justify-center">
             <div className="w-1/3 flex justify-center">
                 <p className="w-13 text-lg text-white ">
-                    Desenvolvido por: <a href="#">Joaquim Pinto</a> e <a href="#">Goku Baiano</a>
+                    Desenvolvido por: <a href="#">André Ofuji</a> e <a href="#">Rogério Miranda</a>
                 </p>
             </div>
         </footer>
