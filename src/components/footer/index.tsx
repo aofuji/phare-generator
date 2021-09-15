b9ab5609ba4f0a95cb98694be1d62c3c2c9f2db4
andre.ofuji <andre.ofuji@gmail.com>
2021-09-14 22:21:27 -0300

    Alterado footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,9 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-indigo-600 items-center w-full h-10 border-t flex flex-col justify-center">
+        <footer className="bg-indigo-600 items-center w-full h-16 border-t flex flex-col justify-center">
             <div className="w-1/3 flex justify-center">
-                <p className="w-13 text-lg text-white ">
+                <p className="w-13 text-lg text-white">
                     Desenvolvido por: <a href="#">André Ofuji</a> e <a href="#">Rogério Miranda</a>
                 </p>
             </div>
